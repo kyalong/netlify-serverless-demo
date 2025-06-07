753a002bf653be3af57ff7892c43abc0b28c52dc
kyalong <kyalong@gmail.com>
2025-06-07 20:49:03 +0800

    1111

--- func/category.js
+++ func/category.js
@@ -1,7 +1,6 @@
 // /.netlify/functions/octo
 
 const fetch = require('node-fetch');
-
 exports.handler = async function (event, context, callback) {
   let url = 'https://www.webdesk.cc/backend/api/v1/tabLink/category/list?type=link';
   let res = await fetch(url);
