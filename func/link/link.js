8901569a4ca05355ad5b79b90ffe2d1bece36cbc
kyalong <kyalong@gmail.com>
2025-06-07 22:48:39 +0800

    22222

---
+++ func/link/link.js
@@ -0,0 +1,38 @@
+// /.netlify/functions/octo
+
+const fetch = require('node-fetch');
+exports.handler = async function (event, context, callback) {
+  let url = 'https://www.webdesk.cc/backend/api/v1/tabLink/category/list?type=link';
+  let res = await fetch(url);
+  let data = await res.json();
+  let categoryData = data.data;
+   if (data.code === 0) {
+                       
+                        const traverse = (arr) => {
+                            return arr.map(item => {
+                                if (item.children.length > 0) {
+                                    item.children = traverse(item.children, item.categoryId);
+                                } else {
+                                    delete item.children;
+                                }
+                                return {
+                                    key: item.categoryId,
+                                    label: item.categoryName,
+                                    title: item.categoryName,
+                                    children: item.children,
+                                    icon: item.iconUrl,
+                                    parentId: item.parentId,
+                                    categoryId: item.categoryId,
+                                    categoryName: item.categoryName,
+                                };
+
+                            });
+                        };
+                        categoryData = traverse(categoryData);
+  }
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(categoryData),
+  });
+};
