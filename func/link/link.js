eb76b1bd65ffdadbdad470bdd4aea2b1581a0ee3
kyalong <kyalong@gmail.com>
2025-06-07 23:05:47 +0800

    www

--- func/link/link.js
+++ func/link/link.js
@@ -3,37 +3,16 @@
 const fetch = require('node-fetch');
 exports.handler = async function (event, context, callback) {
     const params = event.queryStringParameters;
-//   let url = 'https://www.webdesk.cc/backend/api/v1/tabLink/category/list?type=link';
-//   let res = await fetch(url);
-//   let data = await res.json();
-//   let categoryData = data.data;
-//    if (data.code === 0) {
-                       
-//                         const traverse = (arr) => {
-//                             return arr.map(item => {
-//                                 if (item.children.length > 0) {
-//                                     item.children = traverse(item.children, item.categoryId);
-//                                 } else {
-//                                     delete item.children;
-//                                 }
-//                                 return {
-//                                     key: item.categoryId,
-//                                     label: item.categoryName,
-//                                     title: item.categoryName,
-//                                     children: item.children,
-//                                     icon: item.iconUrl,
-//                                     parentId: item.parentId,
-//                                     categoryId: item.categoryId,
-//                                     categoryName: item.categoryName,
-//                                 };
+ let url = `https://www.webdesk.cc/backend/api/v1/tabLink/link/item/${params.categoryId}?page=${params.page}&pageSize=${params.pageSize}`;
+  let res = await fetch(url);
+  let data = await res.json();
 
-//                             });
-//                         };
-//                         categoryData = traverse(categoryData);
-//   }
-
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(params),
+   if (data.code === 0) {
+                      
+                        callback(null, {
+                        statusCode: 200,
+                        body: JSON.stringify(data),
   });
+  }
+
 };
